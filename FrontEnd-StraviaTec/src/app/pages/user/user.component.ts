6dc9b2a35a56bbb4e4040bf97adafea314c98874
Angelo Ortiz Vega <angelo2898@gmail.com>
2020-11-04 15:07:00 -0600

    [UPDATE] The user profile is updated

--- FrontEnd-StraviaTec/src/app/pages/user/user.component.ts
+++ FrontEnd-StraviaTec/src/app/pages/user/user.component.ts
@@ -1,12 +1,62 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
     selector: 'user-cmp',
     moduleId: module.id,
     templateUrl: 'user.component.html'
 })
 
+
 export class UserComponent implements OnInit{
-    ngOnInit(){
+
+  ngOnInit(): void{
+    this.addToGroup(this.all)
+  }
+
+  userImage = "../../assets/img/default-avatar.png";
+  userFullName = "Usuario por Defecto";
+  username = "defaultuser"
+  following = 232;
+  followers = 555;
+  activitiesList = [["cycling","Le dio una vuelta a Puntarenas"],["walking","Caminata durante 1 hora"],
+                    ["swimming","Nadó durante 4 horas"],["running","Corrió los 100 metros en 11 segundos"]];
+  activities = this.activitiesList.length;
+
+  phrase = "«Un hombre puede ser un ingrediente crucial para un equipo, pero un hombre no puede hacer un equipo.» Kareem Abdul-Jabbar.";
+
+  running = "running";
+  cycling = "cycling";
+  swimming = "swimming"
+  walking = "walking";
+  all = "master";
+
+  list = [0,1,2,3];
+  x = this.list.toString();
+
+  flag = 0;
+
+
+  public addToGroup(sport){
+    var htmlList = document.getElementById("list");
+    var newList = document.createElement("newList");
+    newList.className = "list-group";
+    newList.id = "list";
+
+    var cont = 0;
+    while(cont<this.activitiesList.length){
+      if(this.activitiesList[cont][0] == sport || sport == this.all){
+        var element = document.createElement("li");
+        element.className = "list-group-item";
+        element.appendChild(document.createTextNode(this.activitiesList[cont][1]));
+        newList.appendChild(element);
+      }
+      cont++;
     }
+    //alert(htmlList);
+    htmlList.replaceWith(newList);
+    //this.flag = 1;
+  }
+
+
 }
