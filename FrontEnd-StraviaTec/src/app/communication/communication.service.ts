6c11dbdd98ecddc5d69d0e41adeb7a39da102e42
Angelo Ortiz Vega <angelo2898@gmail.com>
2020-11-09 14:29:38 -0600

    [UPDATE] creation of modals to create/update /delete races

--- FrontEnd-StraviaTec/src/app/communication/communication.service.ts
+++ FrontEnd-StraviaTec/src/app/communication/communication.service.ts
@@ -8,8 +8,36 @@ export class CommunicationService {
 
   constructor(private http: HttpClient) { }
 
-  // LOGIN - INICIO DE SESIÓN | VERIFICACIÓN DE USUARIO
-  public verifyUser(username: string, password: string){
+  //CREATE RACE
+  public createRace(race_name, race_date, race_path, activity_type, privacity,race_cost,bank_account,race_category, race_partners){
+    return this.http.post<JSON>("api/organizer/create/race",
+                               {"race_name": race_name, "race_date": race_date, "race_path": race_path,"activity_type": activity_type,
+                                "privacity": privacity, "race_cost": race_cost,"bank_account":bank_account,"race_category": race_category, "race_partners": race_partners}).subscribe(res => {
+                                  alert("Carrera creada exitosamente");
+                                }, error =>{
+                                  alert("Se produjo un error al crear una carrera en la base de datos. Intente más tarde.");
+                                })
+  }
+
+  //UPDATE RACE
+  public updateRace(race_name, race_date, race_path, activity_type, privacity,race_cost,bank_account,race_category, race_partners){
+    return this.http.post<JSON>("api/organizer/update/race",
+                               {"race_name": race_name, "race_date": race_date, "race_path": race_path,"activity_type": activity_type,
+                                "privacity": privacity, "race_cost": race_cost,"bank_account":bank_account,"race_category": race_category, "race_partners": race_partners}).subscribe(res => {
+                                  alert("Carrera actualizada exitosamente");
+                                }, error =>{
+                                  alert("Se produjo un error al actualizar la carrera en la base de datos. Intente más tarde.");
+                                })
+  }
+
+  //DELETE RACE
+  public deleteRace(race_name, race_date){
+    return this.http.post<JSON>("api/organizer/delete/race",
+                              {"race_name": race_name, "race_date": race_date}).subscribe(res => {
+                                alert("Carrera eliminada exitosamente");
+                              }, error =>{
+                                alert("Se produjo un error al eliminar la carrera en la base de datos. Intente más tarde.");
+                              })
   }
 
 }
--- FrontEnd-StraviaTec/src/app/pages/organizer/org-race-management/race-management.component.ts
+++ FrontEnd-StraviaTec/src/app/pages/organizer/org-race-management/race-management.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { CommunicationService } from 'app/communication/communication.service';
 
 @Component({
     selector: 'race-management-cmp',
@@ -7,7 +9,7 @@ import { Component } from '@angular/core';
 })
 
 export class RaceManagementComponent{
-  constructor() {}
+  constructor(private modal:NgbModal, private CS: CommunicationService) {}
 
   race_management_table_titles = [
     ["Nombre","Fecha","Recorrido","Tipo de Actividad","Privacidad","Costo","Cuentas Bancarias","Categorías Disponibles","Patrocinadores"]
@@ -18,5 +20,23 @@ export class RaceManagementComponent{
     ["Carrera La Candelaria", "24/12/2020", "C:\Users\AdminCR\Documents\StraviaTec\FrontEnd-StraviaTec\src\assets\gpx\Lunch_Ride.gpx","Atletismo","Público","5000 Colones exactos", "300000000000, 400000000000","Elite, Master A","StraviaTEC, NorthFace"],
     ["Carrera La Candelaria", "24/12/2020", "C:\Users\AdminCR\Documents\StraviaTec\FrontEnd-StraviaTec\src\assets\gpx\Lunch_Ride.gpx","Atletismo","Público","5000 Colones exactos", "300000000000, 400000000000","Elite, Master A","StraviaTEC, NorthFace"]
   ]
+
+  //SE INICIALIZA LA VENTANA EMERGENTE (pop-up)
+  openModal(content){ this.modal.open(content,{size:'lg', centered:true});}
+
+  //ENVÍ0 DE DATOS DE CARRERA A "COMMUNICATION SERVICE" PARA CREAR CARRERA
+  createRace(race_name, race_date, race_path, activity_type, privacity,race_cost,bank_account,race_category, race_partners){
+    this.CS.createRace(race_name, race_date, race_path, activity_type, privacity,race_cost,bank_account,race_category, race_partners);
+  }
+
+  //ENVÍ0 DE DATOS DE CARRERA A "COMMUNICATION SERVICE" PARA ACTUALIZAR CARRERA
+  updateRace(race_name, race_date, race_path, activity_type, privacity,race_cost,bank_account,race_category, race_partners){
+    this.CS.updateRace(race_name, race_date, race_path, activity_type, privacity,race_cost,bank_account,race_category, race_partners);
+  }
+
+  //ENVÍO DE DATOS DE CARRERA A "COMMUNICATION SERVICE" PARA ELIMINAR CARRERA
+  deleteRace(race_name, race_date){
+    this.CS.deleteRace(race_name, race_date);
+  }
 
 }
