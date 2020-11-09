b2b9eab19af3e21e964d99280f6dc042491f31e9
Jonathan Esquivel <jesquivelsa@gmail.com>
2020-11-08 21:45:00 -0600

    [ADD] Initial Register

--- BackEnd-StraviaTec/Controllers/LoginController.cs
+++ BackEnd-StraviaTec/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using BackEnd_StraviaTec.Models;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,36 +15,57 @@ namespace BackEnd_StraviaTec.Controllers
     [EnableCors(origins: "http://localhost:4200/", headers: "*", methods: "*")]
     public class LoginController : ApiController
     {
+        LoginModel loginModel = new LoginModel();
         NpgsqlConnection connection = new NpgsqlConnection();
 
         [HttpPost]
         [Route("api/Login")]
-        public IHttpActionResult CreateConsumer([FromBody] JObject loginInfo)
+        public IHttpActionResult Login([FromBody] JObject loginInfo)
         {
-            Debug.Print((string)loginInfo["username"]);
+
             connection.ConnectionString = "Username = postgres; Password = 123; Host = localhost; Port = 5432; Database = StraviaTec";
             connection.Open();
-            Debug.Print("Conectado");
-            string query = "select username, u_password from athlete where username = '" + (string)loginInfo["username"] + "'";
-            NpgsqlCommand conector = new NpgsqlCommand(query, connection);
-            try
-            {
-                NpgsqlDataReader dr = conector.ExecuteReader();
-                dr.Read();
-                if (dr[1].ToString() == (string)loginInfo["password"])
-                {
-                    connection.Close();
-                    return Ok("Success");
-                }
-                connection.Close();
-                return BadRequest("Password is incorrect");
-            }
-            catch
-            {
-                connection.Close();
-                return BadRequest("No User Found");
-            }
 
+            string query_athlete = "select username, u_password from athlete where username = '" + (string)loginInfo["username"] + "'";
+            NpgsqlCommand conector_athlete = new NpgsqlCommand(query_athlete, connection);
+
+            string query_organizer = "select username, u_password from organizer where username = '" + (string)loginInfo["username"] + "'";
+            NpgsqlCommand conector_organizer = new NpgsqlCommand(query_organizer, connection);
+
+            if (loginModel.verifyLogin(conector_athlete, (string)loginInfo["password"]))
+            {
+                connection.Close();
+                return Ok("Athlete");
+            }
+            if (loginModel.verifyLogin(conector_organizer, (string)loginInfo["password"]))
+            {
+                connection.Close();
+                return Ok("Organizer");
+            }
+            connection.Close();
+            return BadRequest("Username or password is incorrect");
+        }
+
+        [HttpPost]
+        [Route("api/Register")]
+        public IHttpActionResult Register([FromBody] JObject registerInfo)
+        {
+            connection.ConnectionString = "Username = postgres; Password = 123; Host = localhost; Port = 5432; Database = StraviaTec";
+            connection.Open();
+
+            string query = "select username from athlete where username = '" + (string)registerInfo["username"] + 
+                "' union select username from organizer where username = '" + (string)registerInfo["username"] + "'";
+            NpgsqlCommand conector = new NpgsqlCommand(query, connection);
+            if (loginModel.existsUser(conector))
+            {
+                query = "insert into " + (string)registerInfo["userType"] + " values ('" 
+                    + (string)registerInfo["fName"] + "','" + (string)registerInfo["lName"] + "','" 
+                    + (string)registerInfo["nationality"] + "','" + (string)registerInfo["bDate"] + "'," 
+                    + (string)registerInfo["age"] + ",'" + (string)registerInfo["username"] + "','" + (string)registerInfo["password"] + "');";
+                conector = new NpgsqlCommand(query, connection);
+                return Ok("User created");
+            }
+            return BadRequest("User already exist");
         }
     }
 }
---
+++ BackEnd-StraviaTec/Models/LoginModel.cs
@@ -0,0 +1,44 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackEnd_StraviaTec.Models
+{
+    public class LoginModel
+    {
+        public bool verifyLogin(NpgsqlCommand conector, String password)
+        {
+            try
+            {
+                NpgsqlDataReader dr = conector.ExecuteReader();
+                dr.Read();
+                if (dr[1].ToString() == password)
+                {
+                    return true;
+                }
+                return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool existsUser(NpgsqlCommand conector)
+        {
+            try
+            {
+                NpgsqlDataReader dr = conector.ExecuteReader();
+                dr.Read();
+                return false;
+            }
+            catch
+            {
+                return true;
+            }
+        }
+
+    }
+}
